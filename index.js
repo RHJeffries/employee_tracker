9b6a7fc71111007ff3a4448ebd2c39e6d6683f1a
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-12-04 22:10:04 +1030

    prompts added to index. Classes removed, going another way

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+const connection = require('./config/connection');
+
 // print table package
 const { printTable } = require('console-table-printer');
 
+// access classes
+const Department = require(__dirname + '/classes/Department.js');
+const Role = require(__dirname + '/classes/Role.js');
+const Employee = require(__dirname + '/classes/Employee.js');
+
 // select port to listen to
 const PORT = process.env.PORT || 3001;
+
+
+const userPrompt = () => {
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "promptSelection",
+          message: `What would you like to do?`,
+          choices: [
+            "View All Department",
+            "View All Role",
+            "View All Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update an Employee Role",
+            "EXIT",
+        ],
+      },
+    ])
+}
